9df3892648d1525c8df6fbd158f9bb803509ba2a
Sihe Chen <sihe_chen@u.nus.edu>
2023-07-26 05:11:33 +0000

    wip

---
+++ cmake/exo3.cmake
@@ -0,0 +1,17 @@
+# configuration for straka hydrodynamcis
+
+macro(SET_IF_EMPTY _variable)
+  if("${${_variable}}" STREQUAL "")
+    set(${_variable} ${ARGN})
+  endif()
+endmacro()
+
+# athena variables
+set_if_empty(NUMBER_GHOST_CELLS 2)
+
+# canoe configure
+set(PLANET "Earth")
+set(EOS "shallow_yz")
+set(HYDROSTATIC ON)
+set(CUBED_SPHERE ON)
+set(NETCDF ON)
