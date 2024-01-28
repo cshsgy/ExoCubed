b75857d3d4d9d1de0065681ca9790449e41b03b1
jeffyujianfu <yujianfu@umich.edu>
2024-01-27 22:39:56 -0500

    add comments

--- src/opacity/flux_wavelength.hpp
+++ src/opacity/flux_wavelength.hpp
@@ -4,5 +4,5 @@
 #include <fstream>
 #include <utility>
 
+//read two textfiles and combine the list of data into a pair of vector of double specifically sw_band_flux_HD189_11.txt and wavelengths_GCM_11.txt
 std::pair <std::vector <double>, std::vector <double>> read_file (std::string file1, std::string file2);
-
