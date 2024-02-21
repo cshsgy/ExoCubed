2e8cb474587f6b1a8fb7a6c4dcebb91d6c0fbab7
Linfeng Li <linfel@umich.edu>
2024-02-21 14:55:37 -0500

    add wind vector in plot_isobar.py

--- linfel/plot_isobar.py
+++ linfel/plot_isobar.py
@@ -24,6 +24,13 @@ data = np.mean(temp[timeslices,press_idx,:,:], axis=0)
 mean_vlat = np.mean(vlat[timeslices,press_idx,:,:], axis=0)
 mean_vlon = np.mean(vlon[timeslices,press_idx,:,:], axis=0)
 
+# Downsample the data
+stride = 3 
+lon_downsampled = lon[::stride]
+lat_downsampled = lat[::stride]
+vlat_downsampled = mean_vlat[::stride, ::stride]
+vlon_downsampled = mean_vlon[::stride, ::stride]
+
 # Calculate wind speed
 wind_speed = (mean_vlat**2 + mean_vlon**2)**0.5
 
@@ -43,7 +50,7 @@ pc = plt.pcolor(lon, lat, data, shading='auto')
 plt.colorbar(pc, label='Temperature')
 
 # Plot the wind vectors with quiver
-plt.quiver(lon, lat, mean_vlat, mean_vlon, width=0.001, headwidth=3, headlength=5)
+plt.quiver(lon_downsampled, lat_downsampled, vlat_downsampled, vlon_downsampled, width=0.001, headwidth=5, headlength=7)
 
 # Labels and title
 plt.xlabel('Longitude')
@@ -59,4 +66,4 @@ plt.title(f'Temperature and horizontal winds on the {pressure} Pa isobaric plane
 # Adjust the ticks on the y-axis to be more readable
 #plt.yticks(pressure, labels=np.round(np.log10(pressure), 2))
 
-plt.savefig("images/test6.png",dpi=300)
+plt.savefig("images/test7.png",dpi=300)
